0945bba638f6a4f3ff3c05f0d2d2da5524c7fba4
NSVEGUR <nsvegur@gmail.com>
2022-01-27 11:53:59 +0530

    removed otp verification for dev purpose

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,11 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
-import config from './../config';
 import { userType } from '../schema/user.schema';
 import { loginType } from '../schema/login.schema';
 import user from '../util/userType.util';
 import catchAsync from '../util/catchAsync.util';
 import createUser from '../service/user.service';
 import AppError from '../util/appError.util';
+import { UserModel } from '../model/user.model';
+
+interface userExistanceType {
+	userName: string;
+}
 
 const signup = catchAsync(async function (
 	req: Request<{}, {}, userType>,
@@ -14,18 +18,21 @@ const signup = catchAsync(async function (
 ) {
 	const newUser: any = await createUser(req.body, next);
 	newUser.password = undefined;
-	if (config.NODE_ENV == 'development')
-		return res.status(201).json({
-			status: 'success',
-			message: 'user created successfully',
-			data: {
-				newUser
-			}
-		});
-	else {
-		//Call Next function (Send OTP) in the middleware if User is created successfully
-		next();
-	};
+	const { role } = req.body;
+	const token = await newUser.signToken(role);
+	// if (config.NODE_ENV == 'development')
+	return res.status(201).json({
+		status: 'success',
+		message: 'user created successfully',
+		data: {
+			token,
+			user: newUser
+		}
+	});
+	// else {
+	// 	//Call Next function (Send OTP) in the middleware if User is created successfully
+	// 	next();
+	// };
 });
 
 const login = catchAsync(async function (
@@ -33,36 +40,67 @@ const login = catchAsync(async function (
 	res: Response,
 	next: NextFunction
 ) {
-	const { userName, role, password } = req.body;
-	const User = user(role);
-
-	//1) Don't make any sense but to just avoid ts error
-	if (!User)
-		return next(new AppError('Please provide a Valid role among student, teacher, or admin', 401));
+	const { userName, role, password, mobile } = req.body;
 
 	//2) Check user exist and password is correct
-	const currentUser: any = await User.findOne({ userName: userName }).select('+password');
+	const currentUser: any = await UserModel.findOne({ userName: userName }).select('+password').select('+mobile');
 	if (!currentUser || !await currentUser.correctPassword(password, currentUser.password)) {
 		return next(new AppError('Incorrect User Name or Password', 404));
 	}
 
-	currentUser.password = undefined;
+	if (currentUser.__t != role) {
+		return next(new AppError(`You are not a ${role}`, 404));
+	}
 
-	if (config.NODE_ENV == 'development')
-		return res.status(201).json({
-			status: 'success',
-			message: 'loggedInSuccessfully',
-			data: {
-				currentUser
-			}
-		});
-	else {
-		//Call Next function (Send OTP) in the middleware if User is created successfully
-		next();
-	};
+	if (!await currentUser.correctMobile(mobile, currentUser.mobile)) {
+		return next(new AppError('Not Your Mobile Number', 404));
+	}
+
+
+
+	currentUser.password = undefined;
+	const token = await currentUser.signToken(role);
+	// if (config.NODE_ENV == 'development')
+	return res.status(201).json({
+		status: 'success',
+		message: 'loggedInSuccessfully',
+		data: {
+			token,
+			user: currentUser
+		}
+	});
+	// else {
+	// 	//Call Next function (Send OTP) in the middleware if User is created successfully
+	// 	next();
+	// };
 });
 
+
+const checkUserExistance = catchAsync(async function (
+	req: Request<{}, {}, userExistanceType>,
+	res: Response,
+	next: NextFunction
+) {
+	const { userName } = req.body;
+
+	const currentUser = await UserModel.findOne({ userName: userName });
+
+	if (currentUser) {
+		return res.status(400).json({
+			status: 'fail',
+			message: '💥 Username exist',
+		});
+	} else {
+		return res.status(200).json({
+			status: 'success',
+			message: 'Username available',
+		});
+	}
+});
+
+
 export {
+	checkUserExistance,
 	signup,
 	login
 };
--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -14,21 +14,9 @@ export const userSchema = object({
 		password: string({
 			required_error: "Please provide a password"
 		}).min(8, 'Password is too short, minimum 8 characters is required'),
-		passwordConfirm: string({
-			required_error: "Kindly Confirm your password"
-		}),
-		mobileConfirm: string({
-			required_error: "Kindly Confirm your mobile number"
-		}),
 		role: string({
 			required_error: "Please Provide a role for the user"
 		})
-	}).refine((data) => data.password == data.passwordConfirm, {
-		message: "Passwords do not match",
-		path: ['passwordConfirm']
-	}).refine((data) => data.mobile == data.mobileConfirm, {
-		message: "Mobile numbers do not match",
-		path: ['mobileConfirm']
 	}).refine((data) => validator.isMobilePhone(data.mobile), {
 		message: "Please provide a valid mobile number"
 	}).refine((data) => roles.includes(data.role), {
